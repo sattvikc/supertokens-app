f4a2751d7213ae987baacfef8b6315e6cbc8a7c2
Sattvik Chakravarthy <sattvik@gmail.com>
2024-04-02 18:15:17 +0530

    update app config

--- app/config/appInfo.ts
+++ app/config/appInfo.ts
@@ -1,7 +1,7 @@
 export const appInfo = {
     appName: "SuperTokens Next.js demo app",
-    apiDomain: "http://localhost:3000",
-    websiteDomain: "http://localhost:3000",
+    apiDomain: "https://supertokens-app.sraak.com",
+    websiteDomain: "https://supertokens-app.sraak.com",
     apiBasePath: "/api/auth",
     websiteBasePath: "/auth",
 };
